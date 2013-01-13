f5fa749e59979f8088c80bfe72ea51b77ad9101e
Landon Fuller <landonf@bikemonkey.org>
2013-01-13 13:35:01 -0500

    Move ELF declarations to common header.

--- fatelf/utils/fatelf-haiku.c
+++ fatelf/utils/fatelf-haiku.c
@@ -14,6 +14,7 @@
 #include "fatelf-utils.h"
 
 #include "fatelf-haiku.h"
+#include "elf.h"
 
 #define HAIKU_RSRC_HEADER_MAGIC     0x444f1000
 
@@ -26,115 +27,6 @@
 #define HAIKU_FAT_RSRC_ALIGN        8
 
 #define ALIGN(v, a)     (((v + a - 1) / a) * a)
-
-// Standard ELF definitions.
-#define ELF_MAGIC   "\177ELF"
-
-#define EI_NIDENT   16
-#define EI_CLASS    4
-#define EI_DATA     5
-
-#define PT_NULL     0
-
-#define SHT_NULL        0
-#define SHT_PROGBITS    1
-#define SHT_NOBITS      8
-
-typedef uint32_t    Elf32_Addr;
-typedef uint16_t    Elf32_Half;
-typedef uint32_t    Elf32_Off;
-typedef int32_t     Elf32_Sword;
-typedef uint32_t    Elf32_Word;
-
-struct Elf32_Ehdr {
-    uint8_t     e_ident[EI_NIDENT];
-    Elf32_Half  e_type;
-    Elf32_Half  e_machine;
-    Elf32_Word  e_version;
-    Elf32_Addr  e_entry;
-    Elf32_Off   e_phoff;
-    Elf32_Off   e_shoff;
-    Elf32_Word  e_flags;
-    Elf32_Half  e_ehsize;
-    Elf32_Half  e_phentsize;
-    Elf32_Half  e_phnum;
-    Elf32_Half  e_shentsize;
-    Elf32_Half  e_shnum;
-    Elf32_Half  e_shstrndx;
-};
-
-struct Elf32_Shdr {
-    Elf32_Word  sh_name;
-    Elf32_Word  sh_type;
-    Elf32_Word  sh_flags;
-    Elf32_Addr  sh_addr;
-    Elf32_Off   sh_offset;
-    Elf32_Word  sh_size;
-    Elf32_Word  sh_link;
-    Elf32_Word  sh_info;
-    Elf32_Word  sh_addralign;
-    Elf32_Word  sh_entsize;
-};
-
-struct Elf32_Phdr {
-    Elf32_Word  p_type;
-    Elf32_Off   p_offset;
-    Elf32_Addr  p_vaddr;
-    Elf32_Addr  p_paddr;
-    Elf32_Word  p_filesz;
-    Elf32_Word  p_memsz;
-    Elf32_Word  p_flags;
-    Elf32_Word  p_align;
-};
-
-typedef uint64_t    Elf64_Addr;
-typedef uint64_t    Elf64_Off;
-typedef uint16_t    Elf64_Half;
-typedef uint32_t    Elf64_Word;
-typedef int32_t     Elf64_Sword;
-typedef uint64_t    Elf64_Xword;
-typedef int64_t    Elf64_Sxword;
-
-struct Elf64_Ehdr {
-    uint8_t     e_ident[EI_NIDENT];
-    Elf64_Half  e_type;
-    Elf64_Half  e_machine;
-    Elf64_Word  e_version;
-    Elf64_Addr  e_entry;
-    Elf64_Off   e_phoff;
-    Elf64_Off   e_shoff;
-    Elf64_Word  e_flags;
-    Elf64_Half  e_ehsize;
-    Elf64_Half  e_phentsize;
-    Elf64_Half  e_phnum;
-    Elf64_Half  e_shentsize;
-    Elf64_Half  e_shnum;
-    Elf64_Half  e_shstrndx;
-};
-
-struct Elf64_Shdr {
-    Elf64_Word  sh_name;
-    Elf64_Word  sh_type;
-    Elf64_Xword sh_flags;
-    Elf64_Addr  sh_addr;
-    Elf64_Off   sh_offset;
-    Elf64_Xword sh_size;
-    Elf64_Word  sh_link;
-    Elf64_Word  sh_info;
-    Elf64_Xword sh_addralign;
-    Elf64_Xword sh_entsize;
-};
-
-struct Elf64_Phdr {
-    Elf64_Word  p_type;
-    Elf64_Word  p_flags;
-    Elf64_Off   p_offset;
-    Elf64_Addr  p_vaddr;
-    Elf64_Addr  p_paddr;
-    Elf64_Xword p_filesz;
-    Elf64_Xword p_memsz;
-    Elf64_Xword p_align;
-};
 
 struct elf_table_layout {
     uint64_t offset;
